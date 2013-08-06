cce81107044eb426aa5894b4216f82adf049648d
Michel Vargas <micheldorian91@gmail.com>
2013-08-06 12:30:53 -0500

    me lo suda

---
+++ src/Programa.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+
+public class Programa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		JOptionPane.showInputDialog("Aja");
+	}
+
+}
